71b19c6152ff2147ac3a70fca19a2441ceb7bf63
nguyr <duymisc@gmail.com>
2022-07-10 15:16:37 -0500

    Upadting

--- Project-One/src/main/java/com/models/Account_Type.java
+++ Project-One/src/main/java/com/models/Account_Type.java
@@ -2,24 +2,28 @@ package com.models;
 
 public class Account_Type {
 
-    private Integer acc_id_serial;
+    private Integer acc_id;
 
     private String type;
 
     public Account_Type() {
     }
 
-    public Account_Type(Integer acc_id_serial, String type) {
-        this.acc_id_serial = acc_id_serial;
+    public Account_Type(Integer acc_id, String type) {
+        this.acc_id = acc_id;
         this.type = type;
     }
 
-    public Integer getAcc_id_serial() {
-        return acc_id_serial;
+    public Account_Type(String type) {
+        this.type = type;
     }
 
-    public void setAcc_id_serial(Integer acc_id_serial) {
-        this.acc_id_serial = acc_id_serial;
+    public Integer getAcc_id() {
+        return acc_id;
+    }
+
+    public void setAcc_id(Integer acc_id) {
+        this.acc_id = acc_id;
     }
 
     public String getType() {
